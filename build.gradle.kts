8ba310164f4ce70a5c72e62ea2c5b09bf92fb739
solidogen <solidogen@gmail.com>
2021-05-29 12:13:56 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("multiplatform") version "1.5.0"
+    id("org.jetbrains.compose") version "0.0.0-web-dev-13"
+}
+
+group = "com.solidogen"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+}
+
+kotlin {
+    js(IR) {
+        browser()
+        binaries.executable()
+    }
+    sourceSets {
+        val jsMain by getting {
+            dependencies {
+                implementation(compose.web.core)
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,8 @@
+rootProject.name = "compose-web-playground"
+
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
