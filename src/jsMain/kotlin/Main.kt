676aaea4c2a94a327cbbd6aab3a0f472377074f4
solidogen <solidogen@gmail.com>
2021-05-31 20:53:14 +0200

    Second sample

--- src/jsMain/kotlin/Main.kt
+++ src/jsMain/kotlin/Main.kt
@@ -1,33 +1,27 @@
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.web.css.padding
-import androidx.compose.web.css.px
-import androidx.compose.web.elements.Button
-import androidx.compose.web.elements.Div
-import androidx.compose.web.elements.Span
-import androidx.compose.web.elements.Text
+package com.sample
+
+import androidx.compose.web.css.Style
 import androidx.compose.web.renderComposable
+import com.sample.components.*
+import com.sample.content.*
+import com.sample.style.AppStylesheet
+import org.w3c.dom.HTMLElement
+
 
 fun main() {
-    val count = mutableStateOf(0)
-
     renderComposable(rootElementId = "root") {
-        Div(style = { padding(25.px) }) {
-            Button(attrs = {
-                onClick { count.value = count.value - 1 }
-            }) {
-                Text("-")
-            }
+        Style(AppStylesheet)
 
-            Span(style = { padding(15.px) }) {
-                Text("${count.value}")
+        Layout {
+            Header()
+            MainContentLayout {
+                Intro()
+                ComposeWebLibraries()
+                GetStarted()
+                CodeSamples()
+                JoinUs()
             }
-
-
-            Button(attrs = {
-                onClick { count.value = count.value + 1 }
-            }) {
-                Text("+")
-            }
+            PageFooter()
         }
     }
 }
